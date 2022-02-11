0eb4b47d9ed6f421a2fc75106a9612fb0f216c97
eestela42 <eestela@student.42.fr>
2022-02-11 01:35:11 +0100

    deleted wait before init even threads

--- create_threads.c
+++ create_threads.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_create_threads.c                                :+:      :+:    :+:   */
+/*   create_threads.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 21:25:58 by user42            #+#    #+#             */
-/*   Updated: 2022/01/27 18:45:44 by user42           ###   ########.fr       */
+/*   Updated: 2022/02/11 01:32:00 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	ft_create_threads(t_main *main)
 		pthread_create(&tmp->id, NULL, routine, (void*)tmp);
 		tmp = jump(tmp);
 	}
-	usleep(2000);
+	//usleep(2000);
 	if (main->nbr_philo > 1)
 		tmp = main->first;
 	while (tmp)
